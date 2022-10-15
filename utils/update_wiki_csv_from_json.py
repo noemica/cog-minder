257c5e92a11f741385258c457ada7fe3927b5173
noemica <noemica@protonmail.com>
2022-10-14 21:10:32 -0700

    Add JSON to/from CSV converters for use with spreadsheet doc

---
+++ utils/update_wiki_csv_from_json.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env py
+
+import csv
+import json
+from io import StringIO
+from os import path
+
+wiki_path = path.join(path.dirname(path.realpath(__file__)), '..', 'src', 'json', 'wiki.json')
+csv_path = path.join(path.dirname(path.realpath(__file__)), 'wiki.csv')
+
+# Open/parse files
+with open(wiki_path) as f:
+    wiki_json = json.load(f)
+
+with open(csv_path) as f:
+    wiki_csv = {}
+    for row in csv.DictReader(f):
+        wiki_csv[row['Name']] = row
+
+# Update CSV from JSON
+for bot in wiki_json['Bots']:
+    bot_name = bot['Name']
+    if bot_name in wiki_csv:
+        wiki_csv[bot_name]['Page Type'] = 'Bot'
+        wiki_csv[bot_name]['Content'] = bot['Content']
+    else:
+        new_bot = {
+            'Name': bot_name,
+            'Page Type': 'Bot',
+            'Content': bot['Content']
+        }
+        wiki_csv[bot_name] = new_bot
+
+for part in wiki_json['Parts']:
+    part_name = part['Name']
+    if part_name in wiki_csv:
+        wiki_csv[part_name]['Page Type'] = 'Part'
+        wiki_csv[part_name]['Content'] = part['Content']
+    else:
+        new_part = {
+            'Name': part_name,
+            'Page Type': 'part',
+            'Content': part['Content']
+        }
+        wiki_csv[part_name] = new_part
+
+for location in wiki_json['Locations']:
+    location_name = location['Name']
+    if location_name in wiki_csv:
+        wiki_csv[location_name]['Page Type'] = 'Location'
+        wiki_csv[location_name]['Content'] = location['Content']
+    else:
+        new_location = {
+            'Name': location_name,
+            'Page Type': 'location',
+            'Content': location['Content']
+        }
+        wiki_csv[location_name] = new_location
+
+for other in wiki_json['Other']:
+    other_name = other['Name']
+    if other_name in wiki_csv:
+        wiki_csv[other_name]['Page Type'] = 'Other'
+        wiki_csv[other_name]['Content'] = other['Content']
+    else:
+        new_other = {
+            'Name': other_name,
+            'Page Type': 'other',
+            'Content': other['Content']
+        }
+        wiki_csv[other_name] = new_other
+
+# Write out updated csv
+with open(csv_path, 'w', newline='') as f:
+    writer = csv.DictWriter(f, ['Name', 'Page Type', 'Content'])
+
+    writer.writeheader()
+    sorted_names = sorted(wiki_csv.keys())
+
+    for name in sorted_names:
+        wiki_csv[name]['Content'] = wiki_csv[name]['Content'].replace('\n', '\\n')
+        writer.writerow(wiki_csv[name])
---
+++ utils/update_wiki_json_from_csv.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env py
+
+import csv
+import json
+from io import StringIO
+from os import path
+
+wiki_path = path.join(path.dirname(path.realpath(__file__)), '..', 'src', 'json', 'wiki.json')
+csv_path = path.join(path.dirname(path.realpath(__file__)), 'wiki.csv')
+
+# Open/parse files
+with open(wiki_path) as f:
+    wiki_json = json.load(f)
+
+with open(csv_path) as f:
+    wiki_csv = {}
+    for row in csv.DictReader(f):
+        row['Content'] = row['Content'].replace('\\n', '\n')
+        wiki_csv[row['Name']] = row
+
+# Update JSON from CSV
+for csv_obj in wiki_csv.values():
+    if csv_obj['Page Type'] == 'Bot':
+        json_list = wiki_json['Bots']
+    elif csv_obj['Page Type'] == 'Part':
+        json_list = wiki_json['Parts']
+    elif csv_obj['Page Type'] == 'Location':
+        json_list = wiki_json['Locations']
+    elif csv_obj['Page Type'] == 'Other':
+        json_list = wiki_json['Other']
+    else:
+        print('Found csv object without a valid type {}'.format(csv_obj['Name']))
+        continue
+
+    found = False
+    for json_item in json_list:
+        if json_item['Name'] == csv_obj['Name']:
+            # Found existing item, update content
+            json_item['Content'] = csv_obj['Content']
+            found = True
+            break
+
+    if found:
+        continue
+
+    # Failed to find item, add to end
+    if csv_obj['Page Type'] != 'Other':
+        print('Found new page with type not other {}'.format(csv_obj['Name']))
+        continue
+
+    json_list.append({'Name': csv_obj['Name'], 'Content': csv_obj['Content']})
+
+# Sort all lists
+lists = [wiki_json['Bots'], wiki_json['Parts'], wiki_json['Locations'], wiki_json['Other']]
+for l in lists:
+    l.sort(key=lambda x: x['Name'])
+
+# Save JSON
+with open(wiki_path, 'w') as f:
+    json.dump(wiki_json, f, indent=2)
