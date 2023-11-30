582ca3350509eaffd78a00b98d9e40d9403db470
noemica <noemica@protonmail.com>
2023-11-29 22:42:41 -0700

    Change root to src to fix all paths being src/xyz now

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+    root: "src",
     build: {
+        outDir: path.resolve(__dirname, "dist"),
         rollupOptions: {
             input: {
-                index: "./index.html",
-                about: "./src/about.html",
-                bots: "./src/bots.html",
-                build: "./src/build.html",
-                hacks: "./src/hacks.html",
-                lore: "./src/lore.html",
-                parts: "./src/parts.html",
-                rif: "./src/rif.html",
-                simulator: "./src/simulator.html",
-                wiki: "./src/wiki.html",
+                index: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+                bots: path.resolve(__dirname, "src/bots.html"),
+                build: path.resolve(__dirname, "src/build.html"),
+                hacks: path.resolve(__dirname, "src/hacks.html"),
+                lore: path.resolve(__dirname, "src/lore.html"),
+                parts: path.resolve(__dirname, "src/parts.html"),
+                rif: path.resolve(__dirname, "src/rif.html"),
+                simulator: path.resolve(__dirname, "src/simulator.html"),
+                wiki: path.resolve(__dirname, "src/wiki.html"),
             },
         },
     },
