ca326342870d76d5a19e57b43cd36db66082a97a
noemica <noemica@protonmail.com>
2023-12-30 16:19:48 -0700

    Add sorting to parts

--- src/ts/components/Pages/PartsPage/PartsPage.tsx
+++ src/ts/components/Pages/PartsPage/PartsPage.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useState } from "react";
 
 import { ItemSlot } from "../../../types/itemTypes";
-import { itemData, leetSpeakMatchTransform } from "../../../utilities/common";
+import { gallerySort, getItem, itemData, leetSpeakMatchTransform } from "../../../utilities/common";
 import { useSpoilers } from "../../Effects/useLocalStorageValue";
 import PartsPageInput from "./PartsPageInput";
 import PartsSimpleDisplay from "./PartsSimpleDisplay";
@@ -287,12 +287,264 @@ function filterItemNames(pageState: PartsPageState) {
     return filteredItemNames;
 }
 
+// Sorting functions
+function alphabeticalSort(a: string | undefined, b: string | undefined) {
+    const aValue = typeof a === "string" ? a : "";
+    const bValue = typeof b === "string" ? b : "";
+
+    return aValue.localeCompare(bValue);
+}
+
+function criticalSort(a: string | undefined, b: string | undefined) {
+    function reorderName(criticalString: string | undefined) {
+        if (typeof criticalString != "string") {
+            return "";
+        }
+
+        // Try to parse a [crit]% [critType] b11 string
+        let result = /(\d+)% (\w*)/.exec(criticalString);
+        if (result === null) {
+            // Try to parse a simple number string, use destroy crit by default
+            result = /(\d+)/.exec(criticalString);
+            if (result === null) {
+                return "";
+            }
+            // Format crit % with 3 digits for proper sorting
+            return (
+                "Destroy" + parseInt(result[1]).toLocaleString("en-us", { minimumIntegerDigits: 3, useGrouping: false })
+            );
+        }
+
+        // Format crit % with 3 digits for proper sorting
+        return result[2] + parseInt(result[1]).toLocaleString("en-US", { minimumIntegerDigits: 3, useGrouping: false });
+    }
+
+    const aValue = reorderName(a);
+    const bValue = reorderName(b);
+
+    return aValue.localeCompare(bValue);
+}
+
+function damageSort(a: string, b: string) {
+    function getAverage(damageString: string) {
+        if (typeof damageString != "string" || damageString === "") {
+            return 0;
+        }
+
+        const damageArray = damageString
+            .split("-")
+            .map((s) => s.trim())
+            .map((s) => parseInt(s));
+        return damageArray.reduce((sum, val) => sum + val, 0) / damageArray.length;
+    }
+
+    const aValue = getAverage(a);
+    const bValue = getAverage(b);
+
+    return aValue - bValue;
+}
+
+function heatSort(a: string, b: string) {
+    function getValue(val: string | undefined) {
+        if (val === undefined) {
+            return 0;
+        }
+        const lowerVal = val.toLowerCase();
+        if (lowerVal.startsWith("minimal")) {
+            return 5;
+        }
+        if (lowerVal.startsWith("low")) {
+            return 25;
+        }
+        if (lowerVal.startsWith("medium")) {
+            return 37;
+        }
+        if (lowerVal.startsWith("high")) {
+            return 50;
+        }
+        if (lowerVal.startsWith("massive")) {
+            return 80;
+        }
+        if (lowerVal.startsWith("deadly")) {
+            return 100;
+        }
+
+        return 0;
+    }
+
+    const aValue = getValue(a);
+    const bValue = getValue(b);
+
+    return aValue - bValue;
+}
+
+function integerSort(a: string, b: string) {
+    let aValue = parseInt(a);
+    let bValue = parseInt(b);
+
+    if (isNaN(aValue)) {
+        aValue = 0;
+    }
+    if (isNaN(bValue)) {
+        bValue = 0;
+    }
+
+    return aValue - bValue;
+}
+
+function spectrumSort(a: string, b: string) {
+    function getValue(val: string | undefined) {
+        if (val === undefined) {
+            return 0;
+        }
+        const lowerVal = val.toLowerCase();
+        if (lowerVal.startsWith("wide")) {
+            return 10;
+        }
+        if (lowerVal.startsWith("intermediate")) {
+            return 30;
+        }
+        if (lowerVal.startsWith("narrow")) {
+            return 50;
+        }
+        if (lowerVal.startsWith("fine")) {
+            return 100;
+        }
+
+        return 0;
+    }
+
+    const aValue = getValue(a);
+    const bValue = getValue(b);
+
+    return aValue - bValue;
+}
+
+function sortItemNames(itemNames: string[], pageState: PartsPageState) {
+    // Sorts the collection of item names based on the sort settings
+    const sortKeyMap = {
+        Alphabetical: { key: "name", sort: alphabeticalSort },
+        Gallery: { key: "name", sort: gallerySort },
+        Rating: { key: "rating", sort: integerSort },
+        Size: { key: "size", sort: integerSort },
+        Mass: { key: "mass", sort: integerSort },
+        Integrity: { key: "integrity", sort: integerSort },
+        Coverage: { key: "coverage", sort: integerSort },
+        Arc: { key: "arc", sort: integerSort },
+        Critical: { key: "criticalString", sort: criticalSort },
+        Damage: { keys: ["damage", "explosionDamage"], sort: damageSort },
+        Delay: { key: "delay", sort: integerSort },
+        Disruption: { keys: ["disruption", "explosionDisruption"], sort: integerSort },
+        Drag: { key: "drag", sort: integerSort },
+        "Energy/Move": { key: "energyPerMove", sort: integerSort },
+        "Energy Generation": { key: "energyGeneration", sort: integerSort },
+        "Energy Storage": { key: "energyStorage", sort: integerSort },
+        "Energy Upkeep": { key: "energyUpkeep", sort: integerSort },
+        "Explosion Radius": { key: "explosionRadius", sort: integerSort },
+        Falloff: { key: "falloff", sort: integerSort },
+        "Heat/Move": { key: "heatPerMove", sort: integerSort },
+        "Heat Generation": { key: "heatGeneration", sort: integerSort },
+        "Heat Transfer": { keys: ["heatTransfer", "explosionHeatTransfer"], sort: heatSort },
+        "Matter Upkeep": { key: "matterUpkeep", sort: integerSort },
+        Penalty: { key: "penalty", sort: integerSort },
+        "Projectile Count": { key: "projectileCount", sort: integerSort },
+        Range: { key: "range", sort: integerSort },
+        Recoil: { key: "recoil", sort: integerSort },
+        Salvage: { keys: ["salvage", "explosionSalvage"], sort: integerSort },
+        "Shot Energy": { key: "shotEnergy", sort: integerSort },
+        "Shot Heat": { key: "shotHeat", sort: integerSort },
+        "Shot Matter": { key: "shotMatter", sort: integerSort },
+        Spectrum: { keys: ["spectrum", "explosionSpectrum"], sort: spectrumSort },
+        Support: { key: "support", sort: integerSort },
+        Targeting: { key: "targeting", sort: integerSort },
+        "Time/Move": { key: "timePerMove", sort: integerSort },
+        Waypoints: { key: "waypoints", sort: integerSort },
+    };
+
+    // Do initial sort
+    const isSpreadsheet = pageState.mode === "Spreadsheet";
+    const primaryObject = isSpreadsheet ? sortKeyMap["Gallery"] : sortKeyMap[pageState.primarySort || "Alphabetical"];
+    const primaryKeys: string[] = "key" in primaryObject ? [primaryObject.key] : primaryObject.keys;
+    const primarySort = primaryObject.sort;
+    itemNames.sort((a, b) => {
+        const itemA = getItem(a);
+        const itemB = getItem(b);
+
+        const aKey = primaryKeys.find((key: string) => key in itemA && itemA[key] !== undefined);
+        const bKey = primaryKeys.find((key: string) => key in itemB && itemB[key] !== undefined);
+
+        return primarySort(itemA[aKey!], itemB[bKey!]);
+    });
+
+    if (isSpreadsheet) {
+        // If in spreadsheet view do an initial gallery sort only
+        return itemNames;
+    }
+
+    if (pageState.primarySortDirection === "Descending") {
+        itemNames.reverse();
+    }
+
+    // Do secondary sort if selected
+    if (pageState.secondarySort === undefined) {
+        return itemNames;
+    }
+    const secondaryObject = sortKeyMap[sortKeyMap[pageState.secondarySort]];
+
+    const secondaryKeys = "key" in secondaryObject ? [secondaryObject.key] : secondaryObject.keys;
+    const secondarySort = secondaryObject.sort;
+
+    // Group items based on the initial sort
+    const groupedItemNames = {};
+    const groupedKeys: any[] = [];
+    itemNames.forEach((itemName: string) => {
+        const item = getItem(itemName);
+        const key = primaryKeys.find((key: string) => key in item && item[key] !== undefined);
+        const value = item[key!];
+
+        if (value in groupedItemNames) {
+            groupedItemNames[value].push(itemName);
+        } else {
+            groupedItemNames[value] = [itemName];
+            groupedKeys.push(value);
+        }
+    });
+
+    // Sort subgroups
+    groupedKeys.forEach((key) => {
+        const itemNames = groupedItemNames[key];
+
+        itemNames.sort((a: string, b: string) => {
+            const itemA = getItem(a);
+            const itemB = getItem(b);
+
+            const aKey = secondaryKeys.find((key: string) => key in itemA);
+            const bKey = secondaryKeys.find((key: string) => key in itemB);
+
+            return secondarySort(itemA[aKey], itemB[bKey]);
+        });
+    });
+
+    // Combine groups back into single sorted array
+    const reverseSecondaryGroups = pageState.secondarySortDirection === "Descending";
+    let newItems: string[] = [];
+    groupedKeys.forEach((key) => {
+        if (reverseSecondaryGroups) {
+            groupedItemNames[key].reverse();
+        }
+
+        newItems = newItems.concat(groupedItemNames[key]);
+    });
+
+    return newItems;
+}
+
 export default function PartsPage() {
     const [pageState, setPageState] = useState<PartsPageState>({
         mode: "Simple",
     });
 
-    const itemNames = filterItemNames(pageState);
+    const itemNames = sortItemNames(filterItemNames(pageState), pageState);
 
     let modeNode: ReactNode | undefined;
 
