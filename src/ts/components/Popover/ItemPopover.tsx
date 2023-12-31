99fe81c594d307dfc31ec0c77996f0b89dc34dce
noemica <noemica@protonmail.com>
2023-12-30 17:02:13 -0700

    Change part popups to only be in effect when onscreen, improves performance when searching

--- src/ts/components/Popover/ItemPopover.tsx
+++ src/ts/components/Popover/ItemPopover.tsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
+import TrackVisibility from "react-on-screen";
+
 import { Item } from "../../types/itemTypes";
 import Button from "../Buttons/Button";
 import PartDetails from "../GameDetails/PartDetails";
 import { Popover, PopoverContent, PopoverTrigger } from "./Popover";
 
-export default function ItemPopover({ item, visible = true }: { item: Item; visible?: boolean }) {
+export default function ItemPopover({ item }: { item: Item }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function ItemButton({ isVisible }: { isVisible: boolean }) {
+        const button = (
+            <div>
+                <Button>{item.name}</Button>
+            </div>
+        );
+
+        if (isVisible || isOpen) {
+            return (
+                <Popover
+                    initialOpen={isOpen}
+                    onOpenChange={(open) => {
+                        setIsOpen(open);
+                    }}
+                >
+                    <PopoverTrigger asChild={true}>{button}</PopoverTrigger>
+                    <PopoverContent floatingArrowClassName="part-popover-arrow">
+                        <div className="button-popover part-popover">
+                            <PartDetails item={item} />
+                        </div>
+                    </PopoverContent>
+                </Popover>
+            );
+        } else {
+            return button;
+        }
+    }
+
     return (
-        <Popover>
-            <PopoverTrigger asChild={true}>
-                <div>
-                    <Button>{item.name}</Button>
-                </div>
-            </PopoverTrigger>
-            <PopoverContent floatingArrowClassName="part-popover-arrow">
-                <div className="button-popover part-popover">
-                    <PartDetails item={item} />
-                </div>
-            </PopoverContent>
-        </Popover>
+        <TrackVisibility partialVisibility={true}>
+            {({ isVisible }) => <ItemButton isVisible={isVisible} />}
+        </TrackVisibility>
     );
 }
