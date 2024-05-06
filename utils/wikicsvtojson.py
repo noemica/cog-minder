91dab962ed10268709f0ca7888f9eefb554a191d
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2024-05-06 19:48:53 +0200

    wah

--- utils/wikicsvtojson.py
+++ utils/wikicsvtojson.py
@@ -9,6 +9,7 @@ with open(sys.argv[1], encoding='utf-8') as csvf:
     for rows in csvReader: 
         if rows['Page Type'] not in data:
             data[rows['Page Type']] = []
-        data[rows['Page Type']] = data[rows['Page Type']]+[{'Name': rows['Name'], 'Content': rows['Content']}]
+        data[rows['Page Type']] += [{'Name': rows['Name'], 'Content': rows['Content'].replace('\\n', '\n')}]
+
 with open(sys.argv[2], 'w', encoding='utf-8') as jsonf:
     jsonf.write(json.dumps(data, indent=4))
