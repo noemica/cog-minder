7c86dcbd6e2430ff614d2ecff4a1df255f605ca0
noemica <noemica@protonmail.com>
2024-06-27 21:01:13 -0700

    Add a hash.json file as part of build that contains the current commit hash

---
+++ build.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import { execSync } from "child_process";
+import { writeFileSync } from "fs";
+
+console.log("Building...");
+execSync("yarn vite build --emptyOutDir");
+
+console.log("Writing git hash file...");
+const current_hash = JSON.stringify(execSync("git rev-parse HEAD").toString().trim());
+writeFileSync("./dist/hash.json", `{"hash": "${current_hash}"}`);
