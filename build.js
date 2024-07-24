bf81e93d1d350fd6d0d20a8a60b9884be48dab9b
noemica <noemica@protonmail.com>
2024-07-23 18:20:42 -0700

    Print vite output when building

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 import { writeFileSync } from "fs";
 
 console.log("Building...");
-execSync("yarn vite build --emptyOutDir");
+console.log(execSync("yarn vite build --emptyOutDir").toString());
 
 console.log("Writing git hash file...");
 const current_hash = JSON.stringify(execSync("git rev-parse HEAD").toString().trim());
